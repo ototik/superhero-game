0c5a9ebbd32a6960bec2ef57950f73d46e4d5066
KrNikk <krisko.nikolett@gmail.com>
2020-03-05 17:34:36 +0100

    Health bar updates after json data

--- src/components/Nhp.js
+++ src/components/Nhp.js
@@ -6,9 +6,13 @@ class Nhp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      hp1: 0,
-      hp2: 0
+      hp1: data.hp1,
+      hp2: data.hp2
     };
+  }
+
+  setHp() {
+    this.setState({ hp1: data.hp1, hp2: data.hp2 });
   }
 
   render() {
@@ -19,15 +23,15 @@ class Nhp extends React.Component {
           <div className="player1">
             <p>{data.name1}</p>
             <p>{data.hp1}</p>
-            <div id="meter1" className="meter">
-              <span style={{ width: this.hp1 }}></span>
+            <div className="meter">
+              <span id="meter1" style={{ width: this.state.hp1 + "%" }}></span>
             </div>
           </div>
           <div className="player2">
             <p>{data.name2}</p>
             <p>{data.hp2}</p>
-            <div id="meter2" className="meter">
-              <span style={{ width: this.hp2 }}>></span>
+            <div className="meter">
+              <span id="meter2" style={{ width: this.state.hp2 + "%" }}></span>
             </div>
           </div>
         </div>
